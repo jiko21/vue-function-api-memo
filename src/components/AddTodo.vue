d7e46d8a94f320d013939f66db5f62eb7b5ba0e6
Daiki Kojima <daikikojima0531@icloud.com>
2019-07-22 21:32:33 +0900

    A: component for form

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,28 @@
+<template>
+  <b-input-group class="mt-3">
+    <b-form-input v-model="text" required placeholder="write todo..." />
+    <b-input-group-append>
+      <b-button variant="primary" @click="onAddClick">Add</b-button>
+    </b-input-group-append>
+  </b-input-group>
+</template>
+<script>
+import {value} from 'vue-function-api';
+
+export default {
+  props: ['addTodo'],
+  setup(props) {
+    const text = value('');
+    const onAddClick = () => {
+      if (text.value !== '') {
+        props.addTodo(text.value);
+        text.value = '';
+      }
+    };
+    return {
+      text,
+      onAddClick,
+    };
+  },
+};
+</script>
