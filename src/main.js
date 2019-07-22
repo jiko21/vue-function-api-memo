e167bcbe7298e09f6e22074324166f8a19cbf36d
Daiki Kojima <daikikojima0531@icloud.com>
2019-07-22 21:35:39 +0900

    A: bootstrap and vue-function-api

--- src/main.js
+++ src/main.js
@@ -1,11 +1,17 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import "./registerServiceWorker";
+import Vue from 'vue';
+import BootstrapVue from 'bootstrap-vue';
+import {plugin} from 'vue-function-api';
+import App from './App.vue';
+import router from './router';
+import './registerServiceWorker';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 Vue.config.productionTip = false;
+Vue.use(BootstrapVue);
+Vue.use(plugin);
 
 new Vue({
   router,
-  render: h => h(App)
-}).$mount("#app");
+  render: (h) => h(App),
+}).$mount('#app');
