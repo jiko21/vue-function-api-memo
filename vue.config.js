760092018c2d15c80283c342375652d5e123fdcf
Daiki Kojima <daikikojima0531@icloud.com>
2019-07-22 21:46:22 +0900

    A: vue config

---
+++ vue.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  /* eslint-disable max-len */
+  publicPath: process.env.NODE_ENV === 'production' ? '/vue-function-api-memo/' : '/',
+  assetsDir: '',
+  outputDir: 'dist',
+};
