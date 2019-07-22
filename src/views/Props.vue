d55d032297331c9dd19c71c26da94ccbb09205b9
Daiki Kojima <daikikojima0531@icloud.com>
2019-07-22 21:32:04 +0900

    A: view for Props

---
+++ src/views/Props.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="props">
+    <h1>Props版</h1>
+    <AddTodo :addTodo="addTodo" />
+    <TodoList :todos="todos" :deleteTodo="deleteTodo" />
+  </div>
+</template>
+<script>
+import {value} from 'vue-function-api';
+import AddTodo from '@/components/AddTodo.vue';
+import TodoList from '@/components/TodoList.vue';
+
+export default {
+  components: {
+    AddTodo,
+    TodoList,
+  },
+  setup() {
+    const todos = value([]);
+    const addTodo = (todo) => {
+      todos.value.push(todo);
+    };
+    const deleteTodo = (i) => {
+      todos.value.splice(i, 1);
+    };
+    return {
+      addTodo,
+      deleteTodo,
+      todos,
+    };
+  },
+};
+</script>
+<style lang="scss" scoped>
+.props {
+  margin: 0 20%;
+}
+</style>
