cee568b6463cd2294f9716e59e4e98244bec27e7
Daiki Kojima <daikikojima0531@icloud.com>
2019-07-22 21:35:09 +0900

    A: link for Apps

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,21 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <Link />
     <router-view />
   </div>
 </template>
+<script>
+import Link from '@/components/Link.vue';
 
+export default {
+  components: {
+    Link,
+  },
+};
+</script>
 <style lang="scss">
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
@@ -27,3 +32,9 @@
   }
 }
 </style>
+<script>
+import Link from './components/Link';
+export default {
+  components: {Link},
+};
+</script>
