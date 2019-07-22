3cd74c410fc36412fa0930049577363e69807e69
Daiki Kojima <daikikojima0531@icloud.com>
2019-07-22 21:33:47 +0900

    A: link for pages

---
+++ src/components/Link.vue
@@ -0,0 +1,20 @@
+<template>
+  <b-navbar toggleable="lg" type="dark" variant="dark">
+    <b-navbar-brand href="#">NavBar</b-navbar-brand>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav>
+        <b-nav-item to="/">Normal版</b-nav-item>
+        <b-nav-item to="/props">Props版</b-nav-item>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</template>
+
+<script>
+export default {
+  name: 'Link',
+};
+</script>
